f714a5973ea9e092a87dc82ebe5ceff496772718
Eduard Dev <eduardkiller@hotmail.com>
2021-04-21 12:56:12 -0300

    Update TellCommand.kt

--- src/main/java/net/eduard/chat/command/TellCommand.kt
+++ src/main/java/net/eduard/chat/command/TellCommand.kt
@@ -12,35 +12,41 @@ import org.bukkit.entity.Player
 
 class TellCommand : CommandManager("tell", "privado", "pm", "pv", "t") {
 
-    init{
+    init {
         usage = "/tell <player> <message>"
         description = "Manda mensagem para alguem"
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
-        if (Mine.onlyPlayer(sender)) {
-            val player = sender as Player
-            if (args.size < 2) {
-                sendUsage(sender)
-            } else {
-                if (Mine.existsPlayer(sender, args[0])) {
-                    val target =Mine.getPlayer(args[0])
-                    val fakeTarget =FakePlayer(target)
-                    val message =Extra.getText(1, *args)
-                    if (EduChat.instance.chat.tellDisabled.contains(fakeTarget)) {
-                        sender.sendMessage(ChatMessages.tellDisabled.replace("\$player", target.name))
-                    } else {
-                        EduChat.instance.lastPrivateMessage[player] = target
-                        EduChat.instance.lastPrivateMessage[target] = player
-                        sender.sendMessage(ChatMessages.tellTo
-                                .replace("\$target", target.name)
-                                .replace("$>", "").replace("\$message", message))
-                        target.sendMessage(ChatMessages.tellFrom.replace("\$player", sender.getName())
-                                .replace("$>", "").replace("\$message", message))
-                    }
-                }
-            }
+        if (!Mine.onlyPlayer(sender)) return true
+        val player = sender as Player
+        if (args.size < 2) {
+            sendUsage(sender)
+            return true
         }
+        if (!Mine.existsPlayer(sender, args[0])) return true
+        val target = Mine.getPlayer(args[0])
+        val fakeTarget = FakePlayer(target)
+        val message = Extra.getText(1, *args)
+        if (EduChat.instance.chat.tellDisabled.contains(fakeTarget)) {
+            sender.sendMessage(ChatMessages.tellDisabled.replace("\$player", target.name))
+            return true
+        }
+        EduChat.instance.lastPrivateMessage[player] = target
+        EduChat.instance.lastPrivateMessage[target] = player
+        sender.sendMessage(
+            ChatMessages.tellTo
+                .replace("\$target", target.name)
+                .replace("$>", "").replace("\$message", message)
+        )
+        target.sendMessage(
+            ChatMessages.tellFrom.replace("\$player", sender.getName())
+                .replace("$>", "").replace("\$message", message)
+        )
+
+
+
+
         return true
     }
 }
